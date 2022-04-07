a51f7dc9e084f3684a07444d4f5b454b05e37966
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2022-04-07 09:29:42 -0300

    Add files via upload

---
+++ Cópia de Cópia de goldofredoedolores.sql
@@ -0,0 +1,10 @@
+ create database navio;
+ use navio;
+create table pessoas(
+	nome varchar (30),
+	idade int,
+	sexo char (1),
+	peso decimal(5,2),
+    altura decimal(3,2),
+	nacionalidade varchar(30)
+);       
---
+++ goldofredoedolores2.sql
@@ -0,0 +1,14 @@
+insert into pessoas
+(nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Dolores",25,"f",50.62,1.52,"Mexicana"),
+("Godolores",8,"f",30.25,1.12,"Americana");
+
+
+select * from pessoas;
+
+insert into pessoas
+(nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Goldofredo",25,"m",70.70,1.92,"Espanhol");
+
